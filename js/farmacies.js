76cc8b19ceabcba3581acaf29a0faeafd9916e2c
David Armillas Camins <davidac9@TTC.CAT>
2020-01-13 20:45:30 +0100

    mapa mapbox

---
+++ js/farmacies.js
@@ -0,0 +1,29 @@
+var layerFarmacias;
+var urlFarmacias = "dades/farmacies.geojson";
+
+function addDatosFarmacias() {
+
+    layerFarmacias = new L.GeoJSON.AJAX(urlFarmacias, {
+        onEachFeature: function (feature, layer) {
+            popupContent = "<b>" + feature.properties.EQUIPAMENT + "</b>" +
+                "<br>" + feature.properties.TIPUS_VIA +
+                ". " + feature.properties.NOM_CARRER +
+                " " + feature.properties.NUM_CARRER_1 + "</b>";
+            layer.bindPopup(popupContent);
+        },
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: 6,
+                fillColor: "#00ff00",
+                color: "#ffffff",
+                weight: 3,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+        }
+    }).addTo(map);
+
+    map.setView([41.399733, 2.168598], 13);
+    controlCapas.addOverlay(layerFarmacias,"Farmacias");
+
+}
